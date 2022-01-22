3234c898c13dc2476db97fa6a3888d20c98a246a
Charlie-Brunt <cwhbrunt@gmail.com>
2022-01-22 18:58:53 +0000

    remove bloat

--- wordstatistics.py
+++ wordstatistics.py
@@ -1,4 +1,14 @@
+"""
+Script to read .txt file of five-letter words and assign a score to 
+each word based on the letter frequency in the same or file.
+
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
 def letterFrequency(words):
+    """Takes list of strings and returns letter frequency dictionary"""
     # intialise alphabet dictionary
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     frequencyDict = {}
@@ -12,7 +22,9 @@ def letterFrequency(words):
 
     return frequencyDict
 
+
 def letterProbability(frequencyDict):
+    """Takes letter frequency dictionary and returns probability dictionary"""
     probabilityDict = {}
     total = sum(frequencyDict.values())
     for key in frequencyDict:
@@ -20,31 +32,9 @@ def letterProbability(frequencyDict):
     
     return probabilityDict 
 
-def wordScoresNoDuplicates(words, probabilityDict):
-    """deprecated"""
-    wordScoreDict = {}
-    for word in words:
-        word = word[:-1]  # gets rid of \n in words
-        wordScoreDict[word] = 0
-        lettercount = []
-        for letter in word:
-            if letter in lettercount:
-                duplicates = True
-                break
-            else:
-                duplicates = False
-                lettercount.append(letter)
-                wordScoreDict[word] += probabilityDict[letter]
-        if duplicates==True:
-            wordScoreDict.pop(word)
-
-    sortedWordList = sorted(wordScoreDict.items(), key=lambda x: x[1], reverse=True)
-    # remove words with repeated letters
-
-    return sortedWordList
-
 
 def wordScores(words, probabilityDict):
+    """Takes list of strings and letter probabilities, and returns list of tuples (word, score)"""
     wordScoreDict = {}
     for word in words:
         word = word[:-1]  # gets rid of \n in words
@@ -57,13 +47,14 @@ def wordScores(words, probabilityDict):
                 wordScoreDict[word] += probabilityDict[letter]
                 lettercount.append(letter)
 
+    # sort by score
     sortedWordList = sorted(wordScoreDict.items(), key=lambda x: x[1], reverse=True)
-    # remove words with repeated letters
 
     return sortedWordList
 
 
 def normalisedScores(sortedWordList):
+    """Takes score list and normalises scores based on max and min scores"""
     normalisedWordList = []
     maxScore = sortedWordList[0][1]
     minScore = sortedWordList[-1][1]
@@ -74,18 +65,28 @@ def normalisedScores(sortedWordList):
 
 
 def main():
-    import matplotlib.pyplot as plt
-
     with open('answers_wordlist.txt') as f:
+        answers = f.readlines()
+    with open('complete_wordlist.txt') as f:
         words = f.readlines()
 
-    letters = letterFrequency(words)
-    sortByFrequency = sorted(letters.items(), key=lambda x: x[1], reverse=True)
+    frequencies = letterFrequency(answers)
+    probabilities = letterProbability(frequencies)
+    wordScoreList = normalisedScores(wordScores(words, probabilities))
+
+    sortByFrequency = sorted(frequencies.items(), key=lambda x: x[1], reverse=True)
 
     letterList = [i[0] for i in sortByFrequency]
-    frequencies = [i[1] for i in sortByFrequency]
+    frequencyList = [i[1] for i in sortByFrequency]
 
-    plt.bar(letterList, frequencies)
+    plt.bar(letterList, frequencyList)
+    plt.title("letter vs. frequency")
+    plt.show()
+
+    wordsList = np.linspace(0, 12972, 12972)
+    scoreList = [i[1] for i in wordScoreList]
+    plt.plot(wordsList, scoreList)
+    plt.title("word rank vs. score")
     plt.show()
 
 if __name__ == '__main__':
